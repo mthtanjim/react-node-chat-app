3602c6de6f61639bc0ced5389316dd26a945fbc2
MTH Tanjim <mthtanjim@gmail.com>
2022-11-20 14:59:29 +0600

    all done

--- server/index.js
+++ server/index.js
@@ -6,36 +6,33 @@ const {Server} = require("socket.io")
 
 app.use(cors())
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-//for backend...
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-
-//listening event in socket
 io.on("connection", (socket) => {
-    console.log(`User Connnected:  ${socket.id}`)
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data)
-        console.log(`User with id: ${socket.id} joined room ${data}`)
-    })
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-    socket.on("send_message", (data)=> {
-         console.log(data)
-    })
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-    socket.on("discounnect", ()=> {
-        console.log("User Discounnected", socket.id)
-    })
-
-})
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(4000, () => {
     console.log("SERVER RUNNING AT 4000")
 })
+
